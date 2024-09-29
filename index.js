fae0b1e50cbe0af1e6fbb20d7281baa909c14b8a
brightwillies <bwboakye2@gmail.com>
2024-09-28 21:19:35 -0300

    update

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use('/images', express.static('upload/images'));
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}` // Corrected 'htttp' to 'http'
+        image_url: `https://njs-ecom-api.onrender.com:${port}/images/${req.file.filename}` // Corrected 'htttp' to 'http'
     });
 });
 
